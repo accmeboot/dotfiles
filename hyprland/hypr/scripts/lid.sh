55be2de9790e94b0557de6be65974378b99f4e2d
= <=>
2024-04-07 16:47:15 +0200

    feat: a lot of monitor related changes
    1. Each monitor has 5 dedicated workspaces
    2. On lid close event disables built-in monitor, or suspends if there is
       no external monitor.
    3. Date in the bar opens google calendar in firefox

---
+++ hyprland/hypr/scripts/lid.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+if grep open /proc/acpi/button/lid/LID0/state; then
+  hyprctl keyword monitor "eDP-1, 1920x1200@165,0x0,1"
+else
+
+  if hyprctl monitors | grep -c "Monitor DP-1"; then
+    hyprctl keyword monitor "eDP-1, disable"
+  else
+    systemctl suspend
+  fi
+fi
