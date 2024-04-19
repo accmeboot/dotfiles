8ce180f8aafb74a030c9f9969ff7e105f67172f3
Mikhail Vialov <mikhail_vialov@epam.com>
2024-04-19 13:59:57 +0200

    fix: linting rose pine

--- nvim/lua/accme/themes/rose-pine.lua
+++ nvim/lua/accme/themes/rose-pine.lua
@@ -1,29 +1,29 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      variant = "auto",      -- auto, main, moon, or dawn
-      dark_variant = "main", -- main, moon, or dawn
-      dim_inactive_windows = false,
-      extend_background_behind_borders = false,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			variant = "auto", -- auto, main, moon, or dawn
+			dark_variant = "main", -- main, moon, or dawn
+			dim_inactive_windows = false,
+			extend_background_behind_borders = false,
 
-      enable = {
-        terminal = true,
-        legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
-        migrations = true,         -- Handle deprecated options automatically
-      },
+			enable = {
+				terminal = true,
+				legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
+				migrations = true, -- Handle deprecated options automatically
+			},
 
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = false,
-      },
-    })
+			styles = {
+				bold = true,
+				italic = false,
+				transparency = false,
+			},
+		})
 
-    vim.cmd("colorscheme rose-pine-main")
+		vim.cmd("colorscheme rose-pine-main")
 
-    -- recovering statusline colors when transparency is on
-    -- vim.cmd("highlight Statusline guifg=#908caa guibg=#1f1d2e")
-  end,
+		-- recovering statusline colors when transparency is on
+		-- vim.cmd("highlight Statusline guifg=#908caa guibg=#1f1d2e")
+	end,
 }
