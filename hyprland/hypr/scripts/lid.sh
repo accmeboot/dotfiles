4da7fb87008263a1e249a2e53a9f9a33c9c40a8f
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-25 20:50:04 +0200

    style: waybar

--- hyprland/hypr/scripts/lid.sh
+++ hyprland/hypr/scripts/lid.sh
@@ -4,7 +4,7 @@ if grep open /proc/acpi/button/lid/LID0/state; then
   hyprctl keyword monitor "eDP-1, 1920x1200@165,0x0,1"
 else
 
-  if hyprctl monitors | grep -c "Monitor DP-1"; then
+  if hyprctl monitors | grep -c "Monitor DP-3"; then
     hyprctl keyword monitor "eDP-1, disable"
   else
     systemctl suspend
