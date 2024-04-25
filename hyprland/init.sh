c70c737edbf6bbe5bc6f7015f51dc682330915a6
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-25 20:15:57 +0200

    feat: hypr automation

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -1,3 +1,8 @@
 ln -sfn ~/dotfiles/hyprland/hypr/ ~/.config/hypr
 ln -sfn ~/dotfiles/hyprland/rofi/ ~/.config/rofi
 ln -sfn ~/dotfiles/hyprland/waybar/ ~/.config/waybar
+
+sudo pacman -S waybar rofi bluez bluez-utils blueman brightnessctl wl-clipboard hyprpaper hyprlock xclip nemo nwg-look grim slurp pavucontrol papirus-icon-theme
+
+systemctl enable bluetooth.service
+systemctl start bluetooth.service 
