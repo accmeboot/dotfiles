b4a640e13950c76a9730cac679f611eddc28578c
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-27 12:48:36 +0200

    chore: monitors adjustemnt to hybrid mode

--- hyprland/hypr/scripts/lid.sh
+++ hyprland/hypr/scripts/lid.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-  hyprctl keyword monitor "eDP-1, 1920x1200@165,0x0,1"
+  hyprctl keyword monitor "eDP-2, 1920x1200@165,0x0,1"
 else
 
-  if hyprctl monitors | grep -c "Monitor DP-3"; then
-    hyprctl keyword monitor "eDP-1, disable"
+  if hyprctl monitors | grep -c "Monitor DP-1"; then
+    hyprctl keyword monitor "eDP-2, disable"
   else
     systemctl suspend
   fi
