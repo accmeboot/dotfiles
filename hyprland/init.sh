b0c606fa1bcf075a5f59ba32e87740ecd3150f1f
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-27 19:14:52 +0200

    chore: added dependency for checking updates

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -2,7 +2,7 @@ ln -sfn ~/dotfiles/hyprland/hypr/ ~/.config/hypr
 ln -sfn ~/dotfiles/hyprland/rofi/ ~/.config/rofi
 ln -sfn ~/dotfiles/hyprland/waybar/ ~/.config/waybar
 
-sudo pacman -S waybar rofi bluez bluez-utils blueman brightnessctl wl-clipboard hyprpaper hyprlock xclip nemo nwg-look grim slurp pavucontrol papirus-icon-theme
+sudo pacman -S waybar rofi bluez bluez-utils blueman brightnessctl wl-clipboard hyprpaper hyprlock xclip nemo nwg-look grim slurp pavucontrol papirus-icon-theme pacman-contrib
 
 systemctl enable bluetooth.service
 systemctl start bluetooth.service 
