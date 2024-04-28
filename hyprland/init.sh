e4e3d4ff8d35d7ce646aa9faff62d112b2eb6115
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-28 14:45:04 +0200

    style(waybar): taskbar position & button hovers fix

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -1,8 +1,16 @@
+echo "Linking dotfiles..."
+
+if [ -d ~/.config/hypr ]; then rm -r ~/.config/hypr; fi
 ln -sfn ~/dotfiles/hyprland/hypr/ ~/.config/hypr
+
+if [ -d ~/.config/rofi ]; then rm -r ~/.config/rofi; fi
 ln -sfn ~/dotfiles/hyprland/rofi/ ~/.config/rofi
+
+if [ -d ~/.config/waybar ]; then rm -r ~/.config/waybar; fi
 ln -sfn ~/dotfiles/hyprland/waybar/ ~/.config/waybar
 
-sudo pacman -S waybar rofi bluez bluez-utils blueman brightnessctl wl-clipboard hyprpaper hyprlock xclip nemo nwg-look grim slurp pavucontrol papirus-icon-theme pacman-contrib
+echo "Installing dependencies..."
+sudo pacman -S waybar rofi bluez bluez-utils blueman brightnessctl wl-clipboard hyprpaper hyprlock xclip nemo nwg-look grim slurp pavucontrol papirus-icon-theme pacman-contrib cmake cpio meson
 
 systemctl enable bluetooth.service
 systemctl start bluetooth.service 
