5084aae94c7ccca37437c9ec6040f14892c3d1e2
Milhail Vyalov <mishavjalv48@gmail.com>
2024-04-29 20:09:34 +0200

    feat: mac automation

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -10,4 +10,22 @@ ln -sfn $HOME/dotfiles/macos/yabai/ $HOME/.config/yabai
 if [ -d $HOME/.config/skhd ]; then rm -r $HOME/.config/skhd; fi
 ln -sfn $HOME/dotfiles/macos/skhd/ $HOME/.config/skhd
 
+echo "Installing dependencies..."
+dependencies=(koekeishiya/formulae/yabai koekeishiya/formulae/skhd nvim tmux kitty yazi ffmpegthumbnailer unar jq poppler fd ripgrep fzf zoxide)
+installed_formulas=$(brew list --formula)
+
+for pkg in "${dependencies[@]}"; do
+  if ! echo "$installed_formulas" | grep -q "$(basename "$pkg")\$"; then
+    brew install $pkg
+  else
+    echo "$pkg is already installed"
+  fi
+done
+
+echo "Enabling yabai..."
+yabai --start-service
+
+echo "Starting skhd..."
+skhd --start-service
+
 echo "All done!"
