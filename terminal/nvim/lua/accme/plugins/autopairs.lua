830ded7489b76edb0d422ab14cd41241e615e6ff
Milhail Vyalov <mishavjalv48@gmail.com>
2024-05-06 11:42:10 +0200

    chore: lazy sync && no autopairs && starhip dir fix

--- terminal/nvim/lua/accme/plugins/autopairs.lua
+++ terminal/nvim/lua/accme/plugins/autopairs.lua
@@ -1,19 +1,20 @@
 return {
-	"windwp/nvim-autopairs",
-	dependencies = { "windwp/nvim-ts-autotag" },
-	config = function()
-		local autopairs = require("nvim-autopairs")
-		autopairs.setup({
-			check_ts = true,
-			ts_config = {
-				lua = { "string" },
-				javascript = { "template_string" },
-			},
-		})
-
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
-		-- make autopairs and completion work together
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end,
+  "windwp/nvim-autopairs",
+  dependencies = { "windwp/nvim-ts-autotag" },
+  lazy = "verylazy",
+  config = function()
+    -- local autopairs = require("nvim-autopairs")
+    -- autopairs.setup({
+    -- 	check_ts = true,
+    -- 	ts_config = {
+    -- 		lua = { "string" },
+    -- 		javascript = { "template_string" },
+    -- 	},
+    -- })
+    --
+    -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    -- local cmp = require("cmp")
+    -- -- make autopairs and completion work together
+    -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
