b0ad295558e09f514830bd09b3378a8999155b32
Milhail Vyalov <mishavjalv48@gmail.com>
2024-05-06 13:19:01 +0200

    feat: tmux resurect

--- linux/hyprland/hypr/scripts/monitors.sh
+++ linux/hyprland/hypr/scripts/monitors.sh
@@ -2,5 +2,5 @@
 
 
 sleep 2
-python3 $HOME/dotfiles/linux/hyprland/hypr/scripts/monitors.py --direction left --limit-rate 240
+python3 $HOME/dotfiles/linux/hyprland/hypr/scripts/monitors.py --direction right --limit-rate 240
 
