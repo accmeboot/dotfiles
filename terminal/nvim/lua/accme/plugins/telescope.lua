e5a89b1b95dda585e018856acdf68b43e2dc054a
Mikhail Vialov <mikhail_vialov@epam.com>
2024-05-14 12:57:05 +0200

    feat(telescope): include hidden (dotfiles) files into searches

--- terminal/nvim/lua/accme/plugins/telescope.lua
+++ terminal/nvim/lua/accme/plugins/telescope.lua
@@ -33,6 +33,17 @@ return {
 		end
 
 		telescope.setup({
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+				grep_string = {
+					additional_args = { "--hidden" },
+				},
+				live_grep = {
+					additional_args = { "--hidden" },
+				},
+			},
 			defaults = {
 				sorting_strategy = "ascending",
 				results_title = false,
