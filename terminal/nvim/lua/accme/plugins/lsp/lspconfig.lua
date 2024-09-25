8fca86cce657b51e11d72b740500c2c98b3c12a3
Mikhail Vialov <mikhail_vialov@epam.com>
2024-09-25 20:01:33 +0200

    chore(nvim): moving to ts_ls from tsserver, lazy sync

--- terminal/nvim/lua/accme/plugins/lsp/lspconfig.lua
+++ terminal/nvim/lua/accme/plugins/lsp/lspconfig.lua
@@ -1,135 +1,135 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-    {
-      "mrcjkb/rustaceanvim",
-      version = "^4", -- Recommended
-      ft = { "rust" },
-    },
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{
+			"mrcjkb/rustaceanvim",
+			version = "^4", -- Recommended
+			ft = { "rust" },
+		},
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    local on_attach = function(_, bufnr)
-      local opts = { noremap = true, silent = true, buffer = bufnr }
+		local on_attach = function(_, bufnr)
+			local opts = { noremap = true, silent = true, buffer = bufnr }
 
-      -- set keybinds
-      keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)         -- go to definition
-      keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)               -- show definition
-      keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.references()<CR>", opts)         -- show references
-      keymap.set("n", "<leader>C", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- show references
-      keymap.set("v", "<leader>R", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)      -- rename everywhere
-      keymap.set("n", "gr", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)      -- show diagnostic
-      keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-      keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    end
+			-- set keybinds
+			keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- go to definition
+			keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) -- show definition
+			keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.references()<CR>", opts) -- show references
+			keymap.set("n", "<leader>C", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- show references
+			keymap.set("v", "<leader>R", "<cmd>lua vim.lsp.buf.rename()<CR>", opts) -- rename everywhere
+			keymap.set("n", "gr", "<cmd>lua vim.diagnostic.open_float()<CR>", opts) -- show diagnostic
+			keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+			keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+		end
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- LSP config is the same + capabilities
-    vim.g.rustaceanvim = {
-      tools = {},
-      server = {
-        on_attach = on_attach,
-        default_settings = {
-          ["rust-analyzer"] = {
-            diagnostics = {
-              enable = true,
-            },
-            checkOnSave = {
-              command = "clippy",
-            },
-          },
-        },
-      },
-      dap = {},
-    }
+		-- LSP config is the same + capabilities
+		vim.g.rustaceanvim = {
+			tools = {},
+			server = {
+				on_attach = on_attach,
+				default_settings = {
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = true,
+						},
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			},
+			dap = {},
+		}
 
-    lspconfig["html"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetype = { "html", "tmpl", "gotmpl" },
-    })
+		lspconfig["html"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetype = { "html", "tmpl", "gotmpl" },
+		})
 
-    lspconfig["gopls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      cmd = { "gopls", "--remote=auto" },
-    })
+		lspconfig["gopls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			cmd = { "gopls", "--remote=auto" },
+		})
 
-    lspconfig["sqlls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["sqlls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["tsserver"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["ts_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["cssls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["cssls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["tailwindcss"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["tailwindcss"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["emmet_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-    })
+		lspconfig["emmet_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+		})
 
-    lspconfig["svelte"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["svelte"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig.mdx_analyzer.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      root_dir = function(fname)
-        return lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
-      end,
-    })
+		lspconfig.mdx_analyzer.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			root_dir = function(fname)
+				return lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
+			end,
+		})
 
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
 
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["pyright"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    vim.diagnostic.config({
-      virtual_text = {
-        prefix = "",
-        spacing = 4,
-      },
-    })
-  end,
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = "",
+				spacing = 4,
+			},
+		})
+	end,
 }
--- terminal/nvim/lua/accme/plugins/lsp/mason.lua
+++ terminal/nvim/lua/accme/plugins/lsp/mason.lua
@@ -1,46 +1,46 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
-  config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason = require("mason")
 
-    mason.setup({
-      ui = {
-        border = "rounded",
-      },
-    })
+		mason.setup({
+			ui = {
+				border = "rounded",
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "emmet_ls",
-        "gopls",
-        "pyright",
-        "sqlls",
-        "svelte",
-        "rust_analyzer",
-        "mdx_analyzer",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"emmet_ls",
+				"gopls",
+				"pyright",
+				"sqlls",
+				"svelte",
+				"rust_analyzer",
+				"mdx_analyzer",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
 
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "eslint",
-        "prettier",
-        "stylelint",
-        "eslint_d",
-        "prettierd",
-        "goimports",
-        "pylint",
-      },
-    })
-  end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"eslint",
+				"prettier",
+				"stylelint",
+				"eslint_d",
+				"prettierd",
+				"goimports",
+				"pylint",
+			},
+		})
+	end,
 }
