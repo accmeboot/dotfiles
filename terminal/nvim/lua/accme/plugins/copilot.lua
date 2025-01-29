e3eda01fbd39fe944c7b10380606c4dba2dc730c
Mikhail Vialov <mikhail_vialov@epam.com>
2025-01-29 01:11:08 +0100

    (feat): Copilot config & source & model

--- terminal/nvim/lua/accme/plugins/copilot.lua
+++ terminal/nvim/lua/accme/plugins/copilot.lua
@@ -1,19 +1,18 @@
 return {
 	"CopilotC-Nvim/CopilotChat.nvim",
-	dependencies = { "github/copilot.vim" },
+	dependencies = { "zbirenbaum/copilot.lua" },
 	branch = "main",
 	config = function()
 		local chat = require("CopilotChat")
+
 		chat.setup({
 			show_help = false, -- Show help text for CopilotChatInPlace, default: yes
 			debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
 			disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
 			language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
-			-- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-			-- temperature = 0.1,
 			window = {
-				layout = "horizontal",
-				border = "rounded",
+				layout = "vertical", -- 'vertical', 'horizontal', 'float', 'replace'
+				width = 0.3, -- fractional width of parent, or absolute width in columns when > 1
 			},
 			mappings = {
 				complete = {
@@ -21,6 +20,7 @@ return {
 					insert = "<C-c>",
 				},
 			},
+			model = "claude-3.5-sonnet",
 		})
 
 		local function open_h_split()
