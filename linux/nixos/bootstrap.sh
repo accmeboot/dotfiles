d496c8f4c5f359f7c7b104051c80af7c67b1a67f
accmeboot <mishavjalv48@gmail.com>
2025-02-02 20:42:49 +0100

    (feat): new tmux theme

--- linux/nixos/bootstrap.sh
+++ linux/nixos/bootstrap.sh
@@ -5,12 +5,12 @@ set -e
 echo "Linking dotfiles..."
 
 if [ -d $HOME/.config/hypr ]; then rm -r $HOME/.config/hypr; fi
-ln -sfn $HOME/dotfiles/linux/nixosrom/hyprland/hypr/ $HOME/.config/hypr
+ln -sfn $HOME/dotfiles/linux/nixos/hypr/ $HOME/.config/hypr
 
 if [ -d $HOME/.config/rofi ]; then rm -r $HOME/.config/rofi; fi
-ln -sfn $HOME/dotfiles/linux/nixosrom/hyprland/rofi/ $HOME/.config/rofi
+ln -sfn $HOME/dotfiles/linux/nixos/rofi/ $HOME/.config/rofi
 
 if [ -d $HOME/.config/waybar ]; then rm -r $HOME/.config/waybar; fi
-ln -sfn $HOME/dotfiles/linux/nixosrom/hyprland/waybar/ $HOME/.config/waybar
+ln -sfn $HOME/dotfiles/linux/nixos/waybar/ $HOME/.config/waybar
 
 echo "All done!"
