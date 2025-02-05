14d174cd7cc9483f17ad60d4626313b87dd1156e
accmeboot <mishavjalv48@gmail.com>
2025-02-06 00:42:17 +0100

    (feat): audio info in the statusbar

--- linux/nixos/sway/scripts/temps.sh
+++ linux/nixos/sway/scripts/temps.sh
@@ -4,7 +4,13 @@ CPU_TEMP=$(sensors | grep 'Tctl:' | awk '{print $2}' | sed 's/[^0-9.]*//g')
 # Get GPU temperature
 GPU_TEMP=$(sensors | awk '/junction/ {if (!found) {print $2; found=1}}')
 
+# Get audio volume
+VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep 'Volume:' | awk '{print $2 * 100}')
+
+# Get audio status
+IS_MUTED=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -c MUTED)
+
 DATE=$(date "+%H:%M:%S")
 
 # Print the temperatures
-echo "  +${CPU_TEMP}°C  󰘚 ${GPU_TEMP}  ${DATE} "
+echo "  +${CPU_TEMP}°C  󰘚 ${GPU_TEMP}  ${DATE} $(if [ $IS_MUTED = 1 ]; then echo " "; else echo " "; fi)${VOLUME}%"
