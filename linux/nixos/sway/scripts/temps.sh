38f623ca2c94f682f96ce1b4458cc541c92fed74
accmeboot <mishavjalv48@gmail.com>
2025-02-06 01:15:11 +0100

    (feat): audio status in the sway bar

--- linux/nixos/sway/scripts/temps.sh
+++ linux/nixos/sway/scripts/temps.sh
@@ -4,13 +4,16 @@ CPU_TEMP=$(sensors | grep 'Tctl:' | awk '{print $2}' | sed 's/[^0-9.]*//g')
 # Get GPU temperature
 GPU_TEMP=$(sensors | awk '/junction/ {if (!found) {print $2; found=1}}')
 
-# Get audio volume
-VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep 'Volume:' | awk '{print $2 * 100}')
-
 # Get audio status
 IS_MUTED=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -c MUTED)
 
+# Get audio device
+AUDIO_DEVICE=$(wpctl status | grep "\*" | head -1 | awk '{for(i=4; i<=NF; i++) printf "%s ", $i; print ""}')
+
+# Get formatted audio device
+FORMATTED_AUDIO_DEVICE=$(echo $AUDIO_DEVICE | awk '{print $1 " " $2 " " $3 " " $NF * 100}')
+
+# Get current time
 DATE=$(date "+%H:%M:%S")
 
-# Print the temperatures
-echo "  +${CPU_TEMP}°C  󰘚 ${GPU_TEMP}  ${DATE} $(if [ $IS_MUTED = 1 ]; then echo " "; else echo " "; fi)${VOLUME}%"
+echo "$(if [ $IS_MUTED = 1 ]; then echo " "; else echo " "; fi) ${FORMATTED_AUDIO_DEVICE}%   +${CPU_TEMP}°C  󰘚 ${GPU_TEMP}  ${DATE} "
