6a28b008fd77fdfe22397d825cda95385a81f9b7
accmeboot <mishavjalv48@gmail.com>
2025-02-20 21:47:33 +0100

    (feat): notifications full config

--- linux/nixos/sway/scripts/volume-notify.sh
+++ linux/nixos/sway/scripts/volume-notify.sh
@@ -4,4 +4,4 @@
 VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%.0f", ($2 * 100 * 100/153)}')
 
 # Send the notification
-notify-send -a volume -h int:value:"$VOLUME" -h string:x-canonical-private-synchronous:volume "            ${VOLUME}%"
+notify-send -a volume -h int:value:"$VOLUME" -h string:x-canonical-private-synchronous:volume " "
