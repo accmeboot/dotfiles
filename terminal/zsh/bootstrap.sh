235f2d0565dd27d23965c4c91e79479a4400240a
Mikhail Vialov <mikhail_vialov@epam.com>
2025-02-20 10:16:34 +0100

    (feat): vi mode in shell

--- terminal/zsh/bootstrap.sh
+++ terminal/zsh/bootstrap.sh
@@ -13,6 +13,9 @@ echo "Adding nv and kitten icat aliases  to ~/.zshrc"
 echo "alias nv='nvim'" >> ~/.zshrc
 echo "alias icat='kitten icat'" >> ~/.zshrc
 
+echo "Setting vi mode in ~/.zshrc"
+echo "set -o vi" >> ~/.zshrc
+
 echo "Cloning zsh-autosuggestions and zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
