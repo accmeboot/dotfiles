5f717b711b2d2e8b4e2e2e02e67ed0b0229c29b2
accmeboot <mishavjalv48@gmail.com>
2025-02-24 00:22:28 +0100

    (feat): using hand made colorscheme & some extra wallpapers

--- linux/nixos/modules/home-manager/stylix.nix
+++ linux/nixos/modules/home-manager/stylix.nix
@@ -4,26 +4,7 @@
    enable = true;
    image = ../../../../assets/wallpapers/nix-wallpaper-binary-black.png;
    polarity = "dark";
-   base16Scheme = {
-     scheme = "Custom Gruvbox";
-     author = "accme";
-     base00 = "282828";
-     base01 = "3a3735";
-     base02 = "3a3735";
-     base03 = "3a3735";
-     base04 = "d4be98";
-     base05 = "d4be98";
-     base06 = "d4be98";
-     base07 = "d4be98";
-     base08 = "ea6962";
-     base09 = "e78a4e";
-     base0A = "d8a657";
-     base0B = "a9b665";
-     base0C = "89b482";
-     base0D = "7daea3";
-     base0E = "d3869b";
-     base0F = "ea6962";
-   };
+   base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
    targets = {
      kitty.enable = false;
