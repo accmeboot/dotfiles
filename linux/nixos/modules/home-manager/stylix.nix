4b777da727065805aa0a8b4ac7dbf280410ab3c5
accmeboot <mishavjalv48@gmail.com>
2025-02-27 18:41:30 +0100

    (chore): stylix theme update

--- linux/nixos/modules/home-manager/stylix.nix
+++ linux/nixos/modules/home-manager/stylix.nix
@@ -7,24 +7,24 @@
 
    # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
    base16Scheme = {
-     scheme = "Custom Gruvbox";
+     scheme = "Dawn";
      author = "accme";
-     base00 = "1C1917"; 
-     base01 = "2D2927"; 
-     base02 = "3E3937"; 
-     base03 = "756E6B"; 
-     base04 = "988F8C"; 
-     base05 = "DBD6D1"; 
-     base06 = "ECE7E2"; 
-     base07 = "FAF5F0"; 
-     base08 = "C5907B"; 
-     base09 = "C4B187"; 
-     base0A = "D7C897"; 
-     base0B = "B8C196"; 
-     base0C = "A699B7"; 
-     base0D = "9F8B96"; 
-     base0E = "B8B0A4"; 
-     base0F = "7B9F7B"; 
+     base00 = "1A1A1A";
+     base01 = "2A2A2A";
+     base02 = "3A3A3A";
+     base03 = "756E6B";
+     base04 = "988F8C";
+     base05 = "CBC6C1";
+     base06 = "DCD7D2";
+     base07 = "EAE5E0";
+     base08 = "C5907B";
+     base09 = "C4B187";
+     base0A = "D7C897";
+     base0B = "B8C196";
+     base0C = "A699B7";
+     base0D = "9F8B96";
+     base0E = "B8B0A4";
+     base0F = "7B9F7B";
    };
 
 
