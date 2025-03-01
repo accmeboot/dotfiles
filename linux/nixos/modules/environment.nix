f0adbb1979a0e15ae78c4e90f9768632d6ab8df8
accmeboot <mishavjalv48@gmail.com>
2025-03-01 16:28:37 +0100

    (feat): wayland wine

--- linux/nixos/modules/environment.nix
+++ linux/nixos/modules/environment.nix
@@ -58,8 +58,10 @@
     transmission_4-gtk # torrent client
 
     (lutris.override {
-     extraLibraries =  pkgs: [];
-     }) # gaming platform
+     extraLibraries =  pkgs: [
+       wineWowPackages.waylandFull
+     ];
+    }) # gaming platform
 
     thunderbird # email client
   ];
