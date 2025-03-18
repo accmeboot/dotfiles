fe88c3fcccff1345341f48ecc5ac677b2c0c29ce
Mikhail Vialov <mikhail_vialov@epam.com>
2025-03-18 19:58:35 +0100

    (chore): rose pine nvim settings

--- modules/home-manager/nvim/src/lua/accme/plugins/rise-pine.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/rise-pine.lua
@@ -2,6 +2,10 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
+		require("rose-pine").setup({
+			extend_background_behind_borders = false,
+		})
+
 		vim.cmd("colorscheme rose-pine")
-	end
+	end,
 }
