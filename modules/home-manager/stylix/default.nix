e1f39d0df090ee5e45edd7bfe400da015813ab42
accmeboot <mishavjalv48@gmail.com>
2025-03-18 19:52:14 +0100

    (feat): rose pine

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -4,8 +4,8 @@
     image = ../../../assets/wallpapers/bay.jpeg;
     polarity = "dark";
 
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/black-metal.yaml";
-    base16Scheme = import ./src/schemes/dawn.nix;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    # base16Scheme = import ./src/schemes/dawn.nix;
 
     targets = {
       kitty.enable = false;
