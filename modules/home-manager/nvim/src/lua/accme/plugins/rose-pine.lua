261ff8c452ac04d0950bd47554d01eafa0f0e9eb
accmeboot <mishavjalv48@gmail.com>
2025-03-23 11:09:46 +0100

    (fix): rose-pine nvim filename

--- modules/home-manager/nvim/src/lua/accme/plugins/rose-pine.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/rose-pine.lua
@@ -1,9 +1,13 @@
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
+  enabled = true,
 	config = function()
 		require("rose-pine").setup({
 			extend_background_behind_borders = false,
+      styles = {
+          transparency = true,
+      },
 		})
 
 		vim.cmd("colorscheme rose-pine")
