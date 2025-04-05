e0fac5761a492c56db4627d77c040c4356f288cd
accmeboot <mishavjalv48@gmail.com>
2025-04-05 02:29:13 +0200

    (chore): wallpapers rotation

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   stylix = {
     enable = true;
-    image = ../../../assets/wallpapers/wp4770730-hunt-showdown-wallpapers.jpg;
+    image = ../../../assets/wallpapers/bay.jpeg;
     polarity = "dark";
 
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
