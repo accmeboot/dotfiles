dc95745c95d0b402f66c633aa8301cd7df70ffd7
Mikhail Vialov <mikhail_vialov@epam.com>
2025-04-08 15:59:02 +0200

    (feat): lualine & tmux new theme

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -4,7 +4,7 @@
     image = ../../../assets/wallpapers/bay.jpeg;
     polarity = "dark";
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
     # base16Scheme = import ./src/schemes/dawn.nix;
 
     targets = {
