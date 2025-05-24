b44385ea0b0731764d7254da90d8f2ccbf969791
Mikhail Vialov <mikhail_vialov@epam.com>
2025-05-24 03:16:13 +0200

    (fix): avante markdown

--- modules/home-manager/nvim/src/lua/accme/plugins/avante.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/avante.lua
@@ -22,23 +22,6 @@ return {
 		"stevearc/dressing.nvim",
 		"MunifTanjim/nui.nvim",
 		{
-			-- support for image pasting
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-		},
-		{
 			-- Make sure to set this up properly if you have lazy=true
 			"MeanderingProgrammer/render-markdown.nvim",
 			opts = {
