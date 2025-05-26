9dca92f2ffc85fccefca1fc95b115050cc8346d6
accmeboot <mishavjalv48@gmail.com>
2025-05-26 19:27:03 +0200

    (feat): fastfetch custom logo

--- modules/home-manager/fastfetch/default.nix
+++ modules/home-manager/fastfetch/default.nix
@@ -5,9 +5,12 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-          type = "small";
+          type = "kitty-icat";
+          source = ../../../assets/wallpapers/term-logo.png;
+          width = 18;
+          height = 5;
           padding = {
-            left = 2;
+            left = 1;
             top = 1;
           };
       };
