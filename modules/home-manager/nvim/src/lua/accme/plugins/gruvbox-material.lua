290168b50fafaf2127be0db6d47dd03b6c5b7e07
accmeboot <mishavjalv48@gmail.com>
2025-05-30 16:03:32 +0200

    (feat): nvim gruvbox theme transparency

--- modules/home-manager/nvim/src/lua/accme/plugins/gruvbox-material.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/gruvbox-material.lua
@@ -1,12 +1,14 @@
 return {
-	"sainnhe/gruvbox-material",
-	lazy = false,
-	priority = 1000,
-	enabled = true,
-	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  enabled = true,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_transparent_background = 1
+
+    vim.cmd.colorscheme("gruvbox-material")
+  end,
 }
