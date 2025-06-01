68467ab108f8edf37dac16b31745f1f5c0de7059
accmeboot <mishavjalv48@gmail.com>
2025-06-01 18:44:49 +0200

    (fix): nvim transparency

--- modules/home-manager/nvim/src/lua/accme/plugins/gruvbox-material.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/gruvbox-material.lua
@@ -7,7 +7,7 @@ return {
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_transparent_background = 2
 
     vim.cmd.colorscheme("gruvbox-material")
   end,
--- modules/home-manager/nvim/src/lua/accme/plugins/init.lua
+++ modules/home-manager/nvim/src/lua/accme/plugins/init.lua
@@ -1,6 +1,25 @@
 return {
   "https://github.com/christoomey/vim-tmux-navigator",
   "nvim-tree/nvim-web-devicons",
-  "MeanderingProgrammer/render-markdown.nvim",
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        disable_background = true,
+        style = "normal",
+      },
+      heading = {
+        backgrounds = {
+          'NONE',
+          'NONE',
+          'NONE',
+          'NONE',
+          'NONE',
+          'NONE',
+        },
+      },
+    },
+  },
   "nvim-lua/plenary.nvim",
 }
