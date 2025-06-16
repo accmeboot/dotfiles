3c4e96cb9f217712a706027efa9bef6cd90a17d3
accmeboot <mishavjalv48@gmail.com>
2025-06-17 01:46:18 +0200

    (fix): wallpaper colros

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   stylix = {
     enable = true;
-    image = ../../../assets/wallpapers/Evangelion.png;
+    image = ../../../assets/wallpapers/Evangelion-gruvbox.png;
     polarity = "dark";
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
