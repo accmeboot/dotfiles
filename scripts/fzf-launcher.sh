bb68bbc95ce40d327048db0c75283df01291c463
accmeboot <mishavjalv48@gmail.com>
2025-10-21 00:07:08 +0200

    (fix): fzf line selection highlight

--- scripts/fzf-launcher.sh
+++ scripts/fzf-launcher.sh
@@ -5,7 +5,7 @@ IFS=':'
 get_selection() {
   for p in $PATH; do
     ls "$p"
-  done | sort -u | fzf --print-query --color=16 --no-scrollbar --gutter=' ' | tail -1
+  done | sort -u | fzf --print-query --color=16 --no-scrollbar --gutter=' ' --highlight-line | tail -1
 }
 
 if selection=$( get_selection ); then
