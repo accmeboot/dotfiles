e02e6d68b32624d5707ee3402f54a82f2e86c37d
accmeboot <mishavjalv48@gmail.com>
2025-10-21 12:20:29 +0200

    (chore): scripts refactoring

--- scripts/fzf-launcher.sh
+++ scripts/fzf-launcher.sh
@@ -4,17 +4,7 @@ IFS=':'
 
 SCRIPT_DIR="$HOME/dotfiles/scripts"
 
-cpu_temp=$("$SCRIPT_DIR/cpu-temp.sh" 2>&1)
-gpu_temp=$("$SCRIPT_DIR/gpu-temp.sh" 2>&1)
-ram_usage=$("$SCRIPT_DIR/ram-usage.sh" 2>&1)
-disk_usage=$("$SCRIPT_DIR/disk-usage.sh" 2>&1)
-
-[ -z "$cpu_temp" ] && cpu_temp="N/A"
-[ -z "$gpu_temp" ] && gpu_temp="N/A"
-[ -z "$ram_usage" ] && ram_usage="N/A"
-[ -z "$disk_usage" ] && disk_usage="N/A"
-
-stats="󰻠 ${cpu_temp}°C 󰢮 ${gpu_temp}°C 󰑭 ${ram_usage}% 󰋊 ${disk_usage}%"
+stats=$("$SCRIPT_DIR/system-monitor.sh" 2>&1)
 
 get_selection() {
   for p in $PATH; do
---
+++ scripts/system-monitor.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_DIR="$HOME/dotfiles/scripts"
+
+
+cpu_temp=$("$SCRIPT_DIR/cpu-temp.sh" 2>&1)
+gpu_temp=$("$SCRIPT_DIR/gpu-temp.sh" 2>&1)
+ram_usage=$("$SCRIPT_DIR/ram-usage.sh" 2>&1)
+disk_usage=$("$SCRIPT_DIR/disk-usage.sh" 2>&1)
+
+[ -z "$cpu_temp" ] && cpu_temp="N/A"
+[ -z "$gpu_temp" ] && gpu_temp="N/A"
+[ -z "$ram_usage" ] && ram_usage="N/A"
+[ -z "$disk_usage" ] && disk_usage="N/A"
+
+stats="󰻠 ${cpu_temp}°C 󰢮 ${gpu_temp}°C 󰑭 ${ram_usage}% 󰋊 ${disk_usage}%"
+
+echo "$stats"
