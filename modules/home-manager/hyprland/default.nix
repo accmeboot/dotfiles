20ae77b9417be35fb591bed08a502b7459884bb3
accmeboot <mishavjalv48@gmail.com>
2025-10-22 19:53:43 +0200

    (feat): animation tweaks

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -83,7 +83,23 @@
       };
 
       animations = {
-        enabled = false;
+        enabled = true;
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
+        animation = [
+          "windows, 1, 4, wind, slide"
+          "windowsIn, 1, 4, winIn, slide"
+          "windowsOut, 1, 3, winOut, slide"
+          "windowsMove, 1, 3, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 13, liner, loop"
+          "fade, 1, 8, default"
+          "workspaces, 1, 3, wind"
+        ];
       };
 
       # Group configuration
