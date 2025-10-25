4b82ecebed4f8834a6c78abfbd015ba9d487c6e4
accmeboot <mishavjalv48@gmail.com>
2025-10-25 17:46:20 +0200

    (fix): tmux logo

--- modules/home-manager/fastfetch/default.nix
+++ modules/home-manager/fastfetch/default.nix
@@ -5,7 +5,7 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-          type = "kitty-direct";
+          type = "none";
           source = ../../../assets/wallpapers/nixos-colors.png;
           width = 24;
           padding = {
