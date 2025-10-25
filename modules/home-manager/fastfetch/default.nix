14ba52d460b2479c536be33550481edc328d46df
accmeboot <mishavjalv48@gmail.com>
2025-10-25 03:53:15 +0200

    (feat): new fastfetch

--- modules/home-manager/fastfetch/default.nix
+++ modules/home-manager/fastfetch/default.nix
@@ -6,55 +6,67 @@
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
           type = "kitty-icat";
-          source = ../../../assets/wallpapers/nixos-icon.svg;
+          source = ../../../assets/wallpapers/nixos-colors.png;
           width = 24;
           height = 24;
       };
       display.separator = " ";
       modules = [
-        "break"
-        "break"
-        {
-          key = "󰅐 ";
-          keyColor = "37";
-          type = "uptime";
-        }
-        {
-          key = " ";
-          keyColor = "31";
-          type = "packages";
-        }
-        {
-          key = " ";
-          keyColor = "32";
-          type = "wm";
-        }
-        {
-          key = " ";
-          keyColor = "33";
-          type = "shell";
-        }
-        {
-          key = " ";
-          keyColor = "34";
-          type = "terminal";
-        }
-        {
-          key = " ";
-          keyColor = "35";
-          type = "disk";
-        }
-        {
-          key = "󰑭 ";
-          keyColor = "36";
-          type = "memory";
-        }
-        "break"
-        {
-          type = "custom";
-          format = "{#90}  {#31}  {#32}  {#33}  {#34}  {#35}  {#36}  {#37} ";
-        }
-        "break"
+          {
+            key = "╭───────────╮";
+            type = "custom";
+          }
+          {
+            key = "│ {#33}󰅐 uptime  {#keys}│";
+            type = "uptime";
+          }
+          {
+            key = "│ {#34}{icon} distro  {#keys}│";
+            type = "os";
+          }
+          {
+            key = "│ {#35} kernel  {#keys}│";
+            type = "kernel";
+          }
+          {
+            key = "│ {#36}󰇄 desktop {#keys}│";
+            type = "de";
+          }
+          {
+            key = "│ {#31} term    {#keys}│";
+            type = "terminal";
+          }
+          {
+            key = "│ {#32} shell   {#keys}│";
+            type = "shell";
+          }
+          {
+            key = "│ {#33}󰍛 cpu     {#keys}│";
+            type = "cpu";
+            showPeCoreCount = true;
+          }
+          {
+            key = "│ {#34}󰉉 disk    {#keys}│";
+            type = "disk";
+            folders = "/";
+          }
+          {
+            key = "│ {#35} memory  {#keys}│";
+            type = "memory";
+          }
+          {
+            key = "├───────────┤";
+            type = "custom";
+          }
+          {
+            key = "│ {#39} colors  {#keys}│";
+            type = "colors";
+            symbol = "circle";
+          }
+          {
+            key = "╰───────────╯";
+            type = "custom";
+          }
       ];
     };
   };
