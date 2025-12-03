b1ad1d1ffb1976829813262ce0e205de4868e657
accmeboot <mishavjalv48@gmail.com>
2025-12-03 01:39:25 +0100

    (feat): dunst theme improved

--- modules/home-manager/dunst/default.nix
+++ modules/home-manager/dunst/default.nix
@@ -12,13 +12,17 @@ in {
 
     settings = {
       global = {
-        width = "(200,400)";
+        width = "350";
+        height = "(0,200)";
         offset = theme.spacing.xxl;
         corner_radius = theme.borderRadius;
         icon_corner_radius = theme.borderRadius;
         frame_width = 0;
         gap_size = theme.spacing.xs;
         max_icon_size = 128;
+        min_icon_size = 64;
+        text_icon_padding = theme.spacing.s;
+        vertical_alignment = "center";
         dmenu = "rofi -p 'Select:' -dmenu";
         show_indicators = true;
         mouse_middle_click = "context";
