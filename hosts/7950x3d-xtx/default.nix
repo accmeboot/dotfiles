9d46ac99dc7ab374ff91001e6299047bcc67d1a5
accmeboot <mishavjalv48@gmail.com>
2026-01-19 18:35:03 +0100

    (fix): solar icon

--- hosts/7950x3d-xtx/default.nix
+++ hosts/7950x3d-xtx/default.nix
@@ -36,7 +36,7 @@
     window =
       "hide"; # Show the window on startup (show, *hide*, only [window only])
     batteryIcons =
-      "solaar"; # Which battery icons to use (*regular*, symbolic, solaar)
+      "symbolic"; # Which battery icons to use (*regular*, symbolic, solaar)
     extraArgs = ""; # Extra arguments to pass to solaar on startup
   };
 }
