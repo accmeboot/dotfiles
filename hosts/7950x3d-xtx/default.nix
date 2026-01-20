f1aaf7561dbaf44a676d2baac355f1a8cbc4c462
accmeboot <mishavjalv48@gmail.com>
2026-01-20 23:47:10 +0100

    (feat): razer is back

--- hosts/7950x3d-xtx/default.nix
+++ hosts/7950x3d-xtx/default.nix
@@ -12,7 +12,8 @@
       enable32Bit = true;
       extraPackages = with pkgs; [ rocmPackages.clr.icd ];
     };
-    logitech.wireless.enable = true;
+    logitech.wireless.enable = false;
+    openrazer = { enable = true; };
   };
 
   system.stateVersion = "24.11";
@@ -26,12 +27,12 @@
     users.accme = {
       isNormalUser = true;
       description = "accme";
-      extraGroups = [ "networkmanager" "wheel" ];
+      extraGroups = [ "networkmanager" "wheel" "openrazer" ];
     };
   };
 
-  services.solaar = { # this is logitech services
-    enable = true;
+  services.solaar = {
+    enable = false;
     package = pkgs.solaar;
     window =
       "hide"; # Show the window on startup (show, *hide*, only [window only])
@@ -39,4 +40,6 @@
       "symbolic"; # Which battery icons to use (*regular*, symbolic, solaar)
     extraArgs = ""; # Extra arguments to pass to solaar on startup
   };
+
+  environment.systemPackages = with pkgs; [ openrazer-daemon razer-cli ];
 }
