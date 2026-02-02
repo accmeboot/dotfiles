e1588823a3314ca4aba1a7560acfc9d6f70a602d
accmeboot <mishavjalv48@gmail.com>
2026-02-02 18:15:01 +0100

    (feat): new walls & bar paddings

--- modules/home-manager/dwm/config.def.h
+++ modules/home-manager/dwm/config.def.h
@@ -7,7 +7,7 @@ static const unsigned int snap = 16;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const int horizpadbar = 0;       /* horizontal padding for statusbar */
-static const int vertpadbar = 12;       /* vertical padding for statusbar */
+static const int vertpadbar = 10;       /* vertical padding for statusbar */
 static const char* fonts[] = {"SFProDisplay Nerd Font:size=12"};
 static const char dmenufont[] = "SFProDisplay Nerd Font:size=12";
 static unsigned int baralpha = 0xe6;
