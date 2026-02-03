79025e5cd009813131f7bd34056b8d37c2568202
accmeboot <mishavjalv48@gmail.com>
2026-02-03 02:54:28 +0100

    (fix): gamescope blurry graphics

--- hosts/shared/configuration.nix
+++ hosts/shared/configuration.nix
@@ -74,6 +74,11 @@
     nix-ld = { enable = true; };
     zsh.enable = true;
     starship.enable = true;
+    gamescope = {
+      enable = true;
+      package = pkgs.gamescope.overrideAttrs
+        (_: { NIX_CFLAGS_COMPILE = [ "-fno-fast-math" ]; });
+    };
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
