69dc2cd1e623694b92fce9e92f14709637c689cc
accmeboot <mishavjalv48@gmail.com>
2026-02-04 18:42:34 +0100

    (feat): restoring default cursor

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -40,12 +40,6 @@
         };
       };
 
-      cursor = lib.mkIf config.stylix.desktop.enableCursor {
-        package = pkgs.capitaine-cursors;
-        name = "capitaine-cursors";
-        size = 16;
-      };
-
       icons = lib.mkIf config.stylix.desktop.enableIcons {
         enable = true;
         package = pkgs.papirus-icon-theme;
