5669776e7f35ae5c8ec3732d2871481ff8e8129d
accmeboot <mishavjalv48@gmail.com>
2026-02-26 05:47:05 +0100

    (chore): walls

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -11,7 +11,7 @@
       enable = true;
 
       polarity = "dark";
-      image = ../../../assets/wallpapers/stairs.png;
+      image = ../../../assets/wallpapers/evangelion.png;
       base16Scheme = import ./schemes/material-darker.nix;
 
       targets = {
