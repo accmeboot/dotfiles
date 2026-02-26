6b29bf7b092ceaea0d4089229bbf8e40e49aedc6
accmeboot <mishavjalv48@gmail.com>
2026-02-26 05:30:58 +0100

    (fix): yazi rounding

--- modules/home-manager/yazi/default.nix
+++ modules/home-manager/yazi/default.nix
@@ -2,24 +2,6 @@
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
-    theme = {
-      status = {
-        sep_left = {
-          close = "";
-          open = "";
-        };
-        sep_right = {
-          open = "";
-          close = "";
-        };
-      };
-      indicator = {
-        padding = {
-          open = "█";
-          close = "█";
-        };
-      };
-    };
     settings = {
       opener = {
         edit = [{
