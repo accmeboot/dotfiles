f806b8f07a21ee5950e4f8ca3477209d2c86fda3
accmeboot <mishavjalv48@gmail.com>
2026-03-03 21:57:43 +0100

    (feat): mpv-picker added subtitile selection

--- scripts/mpv-select.sh
+++ scripts/mpv-select.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-VIDEO=$(find . -maxdepth 2 -type f \( -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.mov" \) | sort -V | fzf --tiebreak=index --prompt="Select Video > ")
+VIDEO=$(find . -maxdepth 2 -type f \( -iname "*.mkv" -o -iname "*.mp4" -o -iname "*.avi" -o -iname "*.mov" \) | sort -V | fzf --tiebreak=index --prompt="1. Select Video > ")
 
 [ -z "$VIDEO" ] && exit
 
-AUDIO=$(find . -maxdepth 2 -type f \( -iname "*.mka" -o -iname "*.mp3" -o -iname "*.aac" -o -iname "*.wav" -o -iname "*.m4a" -o -iname "*.mkv" \) | sort -V | fzf --tiebreak=index --prompt="Select Audio (ESC to skip) > ")
+AUDIO=$(find . -maxdepth 2 -type f \( -iname "*.mka" -o -iname "*.mp3" -o -iname "*.aac" -o -iname "*.wav" -o -iname "*.m4a" -o -iname "*.mkv" \) | sort -V | fzf --tiebreak=index --prompt="2. Select Audio (ESC to skip) > ")
+
+SUBS=$(find . -maxdepth 2 -type f \( -iname "*.srt" -o -iname "*.ass" -o -iname "*.vtt" -o -iname "*.sub" \) | sort -V | fzf --tiebreak=index --prompt="3. Select Subtitles (ESC to skip) > ")
+
+ARGS=()
 
 if [ -n "$AUDIO" ]; then
-    echo "Playing: $VIDEO"
-    echo "With Audio: $AUDIO"
-    mpv "$VIDEO" --audio-file="$AUDIO"
-else
-    echo "Playing: $VIDEO (no external audio)"
-    mpv "$VIDEO"
+    ARGS+=("--audio-file=$AUDIO")
 fi
+
+if [ -n "$SUBS" ]; then
+    ARGS+=("--sub-file=$SUBS")
+fi
+
+echo "Launching: $VIDEO"
+mpv "$VIDEO" "${ARGS[@]}"
