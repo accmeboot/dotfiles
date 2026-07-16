bf070a6b575a4498652d9d5bf7cc32dc3f3a67d8
accmeboot <mishavjalv48@gmail.com>
2026-07-16 20:54:25 +0200

    (fix): tmux colors

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
-let
-  colors = import ../colorschemes/material-darker.nix;
+let colors = import ../colorschemes/material-darker.nix;
 in {
   programs.tmux = {
     enable = true;
@@ -61,8 +60,8 @@ in {
       set-option -g renumber-windows on
 
 
-      set -g pane-border-style fg=#${colors.base05}
-      set -g pane-active-border-style fg=#${colors.base05}
+      set -g pane-border-style fg=${colors.base05}
+      set -g pane-active-border-style fg=${colors.base05}
 
       set -g status-position top
       set -g window-status-separator "  "
@@ -72,18 +71,18 @@ in {
       set -g "status-format[1]" ""
       set -g status 2
 
-      set -g message-style "fg=#${colors.base05},bg=terminal"
-      set -g message-command-style "fg=#${colors.base05},bg=terminal"
+      set -g message-style "fg=${colors.base05},bg=terminal"
+      set -g message-command-style "fg=${colors.base05},bg=terminal"
 
-      set -g status-style "fg=#${colors.base05},bg=default"
+      set -g status-style "fg=${colors.base05},bg=default"
 
       set -g status-left ""
 
-      set -g window-status-format "#[fg=#${colors.base05},bg=terminal]#I:#W"
-      set -g window-status-current-format "#[fg=#${colors.base05},bg=#${colors.base00}]#[fg=#${colors.base00},bg=#${colors.base05}]#I:#W#[fg=#${colors.base05},bg=#${colors.base00}]"
+      set -g window-status-format "#[fg=${colors.base05},bg=terminal]#I:#W"
+      set -g window-status-current-format "#[fg=${colors.base05},bg=${colors.base00}]#[fg=${colors.base00},bg=${colors.base05}]#I:#W#[fg=${colors.base05},bg=${colors.base00}]"
 
       set -g status-right-length 100
-      set -g status-right "#[fg=#${colors.base05},bg=terminal] #S"
+      set -g status-right "#[fg=${colors.base05},bg=terminal] #S"
     '';
 
     plugins = with pkgs.tmuxPlugins; [ vim-tmux-navigator yank resurrect ];
