6c1d4b983e4516260af03e4ac5c5f9806b3a43be
accmeboot <mishavjalv48@gmail.com>
2026-09-16 01:05:22 +0200

    (feat): sway better window highlihgts & nix flake update

--- modules/home-manager/sway/default.nix
+++ modules/home-manager/sway/default.nix
@@ -67,25 +67,25 @@ in
       };
       colors = {
         focused = {
-          border = "#${stylixColors.base0D}";
-          background = "#${stylixColors.base0D}";
-          text = "#${stylixColors.base00}";
-          indicator = "#${stylixColors.base0D}";
-          childBorder = "#${stylixColors.base0D}";
+          border = "#${stylixColors.base01}";
+          background = "#${stylixColors.base01}";
+          text = "#${stylixColors.base05}";
+          indicator = "#${stylixColors.base01}";
+          childBorder = "#${stylixColors.base01}";
         };
         focusedInactive = {
-          border = "#${stylixColors.base03}";
-          background = "#${stylixColors.base03}";
-          text = "#${stylixColors.base05}";
-          indicator = "#${stylixColors.base03}";
-          childBorder = "#${stylixColors.base03}";
+          border = "#${stylixColors.base01}";
+          background = "#${stylixColors.base01}";
+          text = "#${stylixColors.base03}";
+          indicator = "#${stylixColors.base01}";
+          childBorder = "#${stylixColors.base01}";
         };
         unfocused = {
-          border = "#${stylixColors.base03}";
-          background = "#${stylixColors.base03}";
-          text = "#${stylixColors.base05}";
-          indicator = "#${stylixColors.base03}";
-          childBorder = "#${stylixColors.base03}";
+          border = "#${stylixColors.base01}";
+          background = "#${stylixColors.base01}";
+          text = "#${stylixColors.base03}";
+          indicator = "#${stylixColors.base01}";
+          childBorder = "#${stylixColors.base01}";
         };
         urgent = {
           border = "#${stylixColors.base08}";
