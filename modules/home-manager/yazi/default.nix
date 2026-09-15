5b66e74e1dc3a079f07b64e37581c2276003ae06
accmeboot <mishavjalv48@gmail.com>
2026-09-15 16:31:52 +0200

    (feat): better yazi settings

--- modules/home-manager/yazi/default.nix
+++ modules/home-manager/yazi/default.nix
@@ -1,13 +1,32 @@
-{ ... }: {
+{ lib, ... }: {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
     settings = {
       opener = {
-        edit = [{
-          run = ''nvim "$@"'';
-          block = true;
-        }];
+        edit = [
+          {
+            run = ''nvim "$@"'';
+            block = true;
+          }
+        ];
+      };
+    };
+    theme = {
+      icon.dirs = lib.mkForce [ ];
+      indicator.padding = {
+        open = "█";
+        close = "█";
+      };
+      status = {
+        sep_left = {
+          open = "";
+          close = "";
+        };
+        sep_right = {
+          open = "";
+          close = "";
+        };
       };
     };
   };
