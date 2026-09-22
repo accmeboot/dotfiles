383820244ea78416a6c09ca7d7ff03bdf61da07d
accmeboot <mishavjalv48@gmail.com>
2026-09-22 20:57:57 +0200

    (chore): spacing update

--- modules/home-manager/quickshell/default.nix
+++ modules/home-manager/quickshell/default.nix
@@ -45,7 +45,7 @@ in
         name = config.stylix.fonts.sansSerif.name;
         size = config.stylix.fonts.sizes.desktop;
       };
-      spacing = 8;
+      spacing = 10;
       border = 1;
       wallpaper = "${config.stylix.image}";
     };
