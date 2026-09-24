faa9ca286dabe1dea531e909fc4a88a8ae6f3663
accmeboot <mishavjalv48@gmail.com>
2026-09-24 20:21:07 +0200

    (chore): walls rotation

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -1,17 +1,29 @@
-{ pkgs, inputs, lib, config, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}:
 let
   retint = import ./retint.nix { inherit pkgs lib; };
 
-  mkScheme = { base, image, polarity, }: {
-    inherit image polarity;
-    scheme = retint.mkScheme {
-      scheme = base;
-      inherit image;
+  mkScheme =
+    {
+      base,
+      image,
+      polarity,
+    }:
+    {
+      inherit image polarity;
+      scheme = retint.mkScheme {
+        scheme = base;
+        inherit image;
+      };
     };
-  };
 
   darkScheme = mkScheme {
-    image = "${../../../assets/wallpapers/cyberpunk.png}";
+    image = "${../../../assets/wallpapers/nixos.png}";
     base = "${pkgs.base16-schemes}/share/themes/default-dark.yaml";
     polarity = "dark";
   };
@@ -20,16 +32,19 @@ let
     base = "${pkgs.base16-schemes}/share/themes/default-light.yaml";
     polarity = "light";
   };
-in {
+in
+{
 
-  imports = [ inputs.stylix.homeModules.stylix ./polarity-toggle.nix ];
+  imports = [
+    inputs.stylix.homeModules.stylix
+    ./polarity-toggle.nix
+  ];
 
   options = {
     isMacos = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description =
-        "Whether running on macOS to disable Linux-specific theming";
+      description = "Whether running on macOS to disable Linux-specific theming";
     };
   };
 
